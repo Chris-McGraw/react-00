e243c60d9e7e51ba9c496cfa3e728452aa99e4b9
Chris-McGraw <34453103+Chris-McGraw@users.noreply.github.com>
2020-08-08 14:49:24 -0400

    Add files via upload

---
+++ drum_machine.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      power: "on"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, I'm made from JSX.</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(JSX, document.getElementById('root'))
+const domContainer = document.querySelector('#drum_machine');
+ReactDOM.render(<DrumMachine />, domContainer);
