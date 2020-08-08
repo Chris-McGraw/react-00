71ac62778426f7f7f6cdb94d2b53ef7852df790d
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-08 15:49:41 -0400

    create intitial drum-machine element

--- drum_machine.js
+++ drum_machine.js
@@ -11,12 +11,11 @@ class DrumMachine extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, I'm made from JSX.</h1>
+        <div id="drum-machine"></div>
       </div>
     );
   }
 }
 
-// ReactDOM.render(JSX, document.getElementById('root'));
-const domContainer = document.querySelector('#drum_machine');
-ReactDOM.render(<DrumMachine />, domContainer);
+const reactContainer = document.querySelector("#react-container");
+ReactDOM.render(<DrumMachine />, reactContainer);
