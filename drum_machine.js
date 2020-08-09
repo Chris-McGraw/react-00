f92ffd250b8a3b331553c23190a82b55ae9d83fa
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-09 09:19:43 -0400

    add .drum-pad > p element

--- drum_machine.js
+++ drum_machine.js
@@ -101,38 +101,47 @@ class PadContainer extends React.Component {
         <div id="pad-container">
           <div className="drum-pad" id="pad-q" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>Q</p>
           </div>
 
           <div className="drum-pad" id="pad-w" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>W</p>
           </div>
 
           <div className="drum-pad" id="pad-e" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>E</p>
           </div>
 
           <div className="drum-pad" id="pad-a" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>A</p>
           </div>
 
           <div className="drum-pad" id="pad-s" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>S</p>
           </div>
 
           <div className="drum-pad" id="pad-d" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>D</p>
           </div>
 
           <div className="drum-pad" id="pad-z" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>Z</p>
           </div>
 
           <div className="drum-pad" id="pad-x" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>X</p>
           </div>
 
           <div className="drum-pad" id="pad-c" style={padPowered} onClick={this.padPress}>
             <div className="pad-glow" style={padGlowPowered}></div>
+            <p>C</p>
           </div>
         </div>
       </div>
