d7327eeaa1547c4052c7ea6647db43979ee2409a
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-11 12:36:39 -0400

    add kitBtnDown/Up styles to KitChoiceContainer

--- drum_machine.js
+++ drum_machine.js
@@ -111,7 +111,7 @@ class KitChoiceContainer extends React.Component {
   kitBtnDown(event) {
     event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0), inset 0 0 100px 100px rgba(255, 255, 255, 0.5)";
 
-    this.props.setCurrentKit();
+    this.props.setCurrentKit(event);
   }
 
   kitBtnUp(event) {
