9a0a6fc90052d13a086051e22ba0432fd9735e72
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-12 15:49:49 -0400

    export sampleKits from sampleKits.js to drum_mahcine.js

--- drum_machine.js
+++ drum_machine.js
@@ -182,12 +182,12 @@ class PadContainer extends React.Component {
       let audio = document.getElementById(audioID);
 
       if(this.props.currentKit === "kit-1") {
-        console.log(sampleKits[0]);
+        console.log(sampleKits[0].Q);
 
         audio.src = "audio/808s/loaded.wav";
       }
       else if(this.props.currentKit === "kit-2") {
-        console.log(sampleKits[1]);
+        console.log(sampleKits[1][Q]);
 
         audio.src = "audio/808s/chirp.wav";
       }
