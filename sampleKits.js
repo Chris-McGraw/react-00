aeb1728ec756934dd6c479e7635c2d0c67d9a642
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-12 20:02:13 -0400

    Update sampleKits.js

--- sampleKits.js
+++ sampleKits.js
@@ -8,3 +8,16 @@ const sampleKits = [
     W: "audio/808s/chirp.wav"
   }
 ];
+
+const sampleKitsTest = {
+  Kit1: {
+    Q: {src: "audio/808s/loaded.wav",
+    desc: "808s - Loaded"
+    },
+    W: {src: "audio/808s/starburst.wav",
+    desc: "808s - Starburst"
+    },
+  }
+};
+
+console.log(sampleKitsTest.Kit1.Q.src);
