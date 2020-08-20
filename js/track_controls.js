88e533afb6aa6ba3affc09212de42e7b1558d3e1
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-20 17:25:10 -0400

    edit TrackControls component

--- js/track_controls.js
+++ js/track_controls.js
@@ -5,29 +5,47 @@ class TrackControls extends React.Component {
     super(props)
   }
 
+  trackBtnStyle() {
+    if(this.props.power === "on") {
+      return "ctrl-btn ctrl-btn-on";
+    }
+    else {
+      return "ctrl-btn ctrl-btn-off"
+    }
+  }
+
+  trackGlowStyle() {
+    if(this.props.power === "on") {
+      return "ctrl-glow ctrl-glow-on";
+    }
+    else {
+      return "ctrl-glow ctrl-glow-off"
+    }
+  }
+
   render() {
     return (
       <div id="track-controls">
-        <div className="ctrl-btn">
-          <div className="ctrl-glow">
+        <div className={this.trackBtnStyle()}>
+          <div className={this.trackGlowStyle()}>
             <p>I</p>
           </div>
         </div>
 
-        <div className="ctrl-btn">
-          <div className="ctrl-glow">
+        <div className={this.trackBtnStyle()}>
+          <div className={this.trackGlowStyle()}>
             <p>II</p>
           </div>
         </div>
 
-        <div className="ctrl-btn">
-          <div className="ctrl-glow">
+        <div className={this.trackBtnStyle()}>
+          <div className={this.trackGlowStyle()}>
             <p>III</p>
           </div>
         </div>
 
-        <div id="delete-button" className="ctrl-btn">
-          <div className="ctrl-glow">
+        <div id="delete-button" className={this.trackBtnStyle()}>
+          <div className={this.trackGlowStyle()}>
             <i className="fas fa-trash"></i>
           </div>
         </div>
