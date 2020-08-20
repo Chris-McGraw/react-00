1dff27bcb2c951c18334b244db3f83191b66fa90
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-19 21:34:02 -0400

    Revert "edit style rendering conditions for PlaybackControls component"

--- js/kit_choice_container.js
+++ js/kit_choice_container.js
@@ -18,14 +18,14 @@ class KitChoiceContainer extends React.Component {
 
       event.currentTarget.style.backgroundColor = "#dad9de";
       event.currentTarget.style.backgroundImage = "radial-gradient(#E9E8EB, #dad9de)";
-      event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0), inset 0 0 100px 100px rgba(255, 255, 255, 0.2)";
+      event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0)";
 
       this.props.setCurrentKit(event);
     }
   }
 
   kitBtnUp(event) {
-    event.currentTarget.style.boxShadow = "6px 6px 6px rgba(0,0,0, 1.0), inset 0 0 0 0 rgba(255, 255, 255, 0.0)";
+    event.currentTarget.style.boxShadow = "6px 6px 6px rgba(0,0,0, 1.0)";
   }
 
   render() {
@@ -47,19 +47,19 @@ class KitChoiceContainer extends React.Component {
       <div>
         <div id="kit-choice-container">
           <div className="kit-choice-btn" id="kit-btn-1" style={btnPowered}
-          onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp}>
+          onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp} onMouseLeave={this.kitBtnUp}>
             <div className="kit-choice-btn-glow" style={btnGlowPowered}></div>
             <p>1</p>
           </div>
 
           <div className="kit-choice-btn" id="kit-btn-2" style={btnPowered}
-          onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp}>
+          onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp} onMouseLeave={this.kitBtnUp}>
             <div className="kit-choice-btn-glow" style={btnGlowPowered}></div>
             <p>2</p>
           </div>
 
           <div className="kit-choice-btn" id="kit-btn-3" style={btnPowered}
-          onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp}>
+          onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp} onMouseLeave={this.kitBtnUp}>
             <div className="kit-choice-btn-glow" style={btnGlowPowered}></div>
             <p>3</p>
           </div>
