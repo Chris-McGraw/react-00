fb7b105d9ae72f991ccfc71b6d92ab6421ed3211
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-21 16:37:25 -0400

    edit displays component

--- js/displays.js
+++ js/displays.js
@@ -33,6 +33,7 @@ class DisplayLeft extends React.Component {
 
     let test9 = this.props.playbackArr.filter(i => i.key === "Q").map(i => <div key={i.kit + i.key + i.time} className={this.noteBlockStyle()} style={{left: ( (i.time / 1000).toFixed(1) * 10 ) + "%"}}></div>);
     let test8 = this.props.playbackArr.filter(i => i.key === "W").map(i => <div key={i.kit + i.key + i.time} className={this.noteBlockStyle()} style={{left: ( (i.time / 1000).toFixed(1) * 10 ) + "%"}}></div>);
+    let test6 = this.props.playbackArr.filter(i => i.key === "A").map(i => <div key={i.kit + i.key + i.time} className={this.noteBlockStyle()} style={{left: ( (i.time / 1000).toFixed(1) * 10 ) + "%", backgroundColor: sampleKits[i.kit][i.key].noteColor}}></div>);
 
     return (
       <div>
@@ -56,7 +57,7 @@ class DisplayLeft extends React.Component {
           <div className="test-line" id="test-line-3"></div>
           <div className="test-line" id="test-line-4"></div>
           <div className="test-line" id="test-line-5"></div>
-          <div className="test-line" id="test-line-6"></div>
+          <div className="test-line" id="test-line-6">{test6}</div>
           <div className="test-line" id="test-line-7"></div>
           <div className="test-line" id="test-line-8">{test8}</div>
           <div className="test-line" id="test-line-9">{test9}</div>
