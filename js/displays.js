1a8f99efc500c6115bb892ec97964baeb395b8bd
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-21 16:50:47 -0400

    edit displays component

--- js/displays.js
+++ js/displays.js
@@ -5,6 +5,15 @@ class DisplayLeft extends React.Component {
     super(props)
   }
 
+  mapNoteBlocks(key) {
+    return this.props.playbackArr.filter(i =>
+      i.key === key).map(i =>
+        <div key={i.kit + i.key + i.time} className={this.noteBlockStyle()}
+        style={{left: ( (i.time / 1000).toFixed(1) * 10 ) + "%",
+        backgroundColor: sampleKits[i.kit][i.key].noteColor}}></div>
+    );
+  }
+
   noteBlockStyle() {
     if(this.props.power === "on") {
       return "note-block note-block-on";
@@ -31,7 +40,7 @@ class DisplayLeft extends React.Component {
       }
     }
 
-    let test9 = this.props.playbackArr.filter(i => i.key === "Q").map(i => <div key={i.kit + i.key + i.time} className={this.noteBlockStyle()} style={{left: ( (i.time / 1000).toFixed(1) * 10 ) + "%"}}></div>);
+    // let test9 = this.props.playbackArr.filter(i => i.key === "Q").map(i => <div key={i.kit + i.key + i.time} className={this.noteBlockStyle()} style={{left: ( (i.time / 1000).toFixed(1) * 10 ) + "%"}}></div>);
     let test8 = this.props.playbackArr.filter(i => i.key === "W").map(i => <div key={i.kit + i.key + i.time} className={this.noteBlockStyle()} style={{left: ( (i.time / 1000).toFixed(1) * 10 ) + "%"}}></div>);
     let test6 = this.props.playbackArr.filter(i => i.key === "A").map(i => <div key={i.kit + i.key + i.time} className={this.noteBlockStyle()} style={{left: ( (i.time / 1000).toFixed(1) * 10 ) + "%", backgroundColor: sampleKits[i.kit][i.key].noteColor}}></div>);
 
@@ -60,7 +69,7 @@ class DisplayLeft extends React.Component {
           <div className="test-line" id="test-line-6">{test6}</div>
           <div className="test-line" id="test-line-7"></div>
           <div className="test-line" id="test-line-8">{test8}</div>
-          <div className="test-line" id="test-line-9">{test9}</div>
+          <div className="test-line" id="test-line-9">{this.mapNoteBlocks("Q")}</div>
         </div>
       </div>
     );
