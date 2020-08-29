aebb37f3cd8d473d8bafe4c3a2d2c8806a18f7e7
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-29 15:15:48 -0400

    Revert "Update pad_container.js"
    This reverts commit efab019dab200c0195908bab22fa6bba507f0016.

--- js/pad_container.js
+++ js/pad_container.js
@@ -158,7 +158,8 @@ class PadContainer extends React.Component {
   }
 
   mapDrumPads() {
-    return sampleKits.map(i =>
+    return sampleKits.filter(i =>
+      i.kit === "kit1").map(i =>
         <div className="drum-pad" id="pad-q" style={padPowered}
         onMouseDown={this.padPress} onMouseUp={this.padLift} onMouseLeave={this.padLift}>
           <audio preload="auto" src={i.src} className="clip" id="Q"></audio>
