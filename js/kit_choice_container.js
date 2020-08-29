3d03e95fbc4a7e87a1443db244c706ffce1acd15
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-08-29 11:07:37 -0400

    edit style rendering for KitChoiceContainer component

--- js/kit_choice_container.js
+++ js/kit_choice_container.js
@@ -13,11 +13,13 @@ class KitChoiceContainer extends React.Component {
 
       for(let n = 0; n < kitBtns.length; n++) {
         kitBtns[n].style.backgroundColor = "#c0c7ca";
-        kitBtns[n].style.backgroundImage = "radial-gradient(#b6b4be, #c0c7ca)";
+        // kitBtns[n].style.backgroundImage = "radial-gradient(#b6b4be, #c0c7ca)";
+        kitBtns[n].style.backgroundImage = "radial-gradient( rgba(182, 180, 190, 0.8), rgb(192, 199, 202, 0.8) )";
       }
 
       event.currentTarget.style.backgroundColor = "#dad9de";
-      event.currentTarget.style.backgroundImage = "radial-gradient(#E9E8EB, #dad9de)";
+      // event.currentTarget.style.backgroundImage = "radial-gradient(#E9E8EB, #dad9de)";
+      event.currentTarget.style.backgroundImage = "radial-gradient( rgba(233, 232, 235, 0.8), rgba(218, 217, 222, 0.8) )";
       event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0)";
 
       this.props.setCurrentKit(event);
