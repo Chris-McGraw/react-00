8eaa716effaa14dbf69dbf045f6dd3359ed619a0
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-09-01 17:54:41 -0400

    edit style rendering for KitChoiceContainer component

--- js/kit_choice_container.js
+++ js/kit_choice_container.js
@@ -3,6 +3,9 @@
 class KitChoiceContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      kitBtnPressed: ""
+    };
     this.kitBtnDown = this.kitBtnDown.bind(this);
     this.kitBtnUp = this.kitBtnUp.bind(this);
   }
@@ -18,19 +21,32 @@ class KitChoiceContainer extends React.Component {
       //
       // event.currentTarget.style.backgroundColor = "#dad9de";
       // event.currentTarget.style.backgroundImage = "radial-gradient( rgba(233, 232, 235, 0.8), rgba(218, 217, 222, 0.8) )";
-      event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0)";
+      // event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0)";
 
       this.props.setCurrentKit(event);
+
+      this.setState({
+        kitBtnPressed: this.props.currentKit;
+      });
     }
   }
 
   kitBtnUp(event) {
-    event.currentTarget.style.boxShadow = "6px 6px 6px rgba(0,0,0, 1.0)";
+    // event.currentTarget.style.boxShadow = "6px 6px 6px rgba(0,0,0, 1.0)";
+
+    this.setState({
+      kitBtnPressed: "";
+    });
   }
 
   kitBtnStyle(kit) {
     if(this.props.power === "on" && this.props.currentKit === kit) {
-      return "kit-choice-btn ctrl-btn-active"
+      if(this.state.kitBtnPressed === kit) {
+        return "kit-choice-btn ctrl-btn-active kit-btn-pressed"
+      }
+      else {
+        return "kit-choice-btn ctrl-btn-active"
+      }
     }
     else if(this.props.power === "on" && this.props.currentKit !== kit) {
       return "kit-choice-btn ctrl-btn-on"
