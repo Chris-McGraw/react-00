bc41636f5fda48064d61637d91303a9bc7dc3301
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-09-01 17:08:19 -0400

    edit style rendering for KitChoiceContainer component

--- js/kit_choice_container.js
+++ js/kit_choice_container.js
@@ -9,15 +9,15 @@ class KitChoiceContainer extends React.Component {
 
   kitBtnDown(event) {
     if(this.props.power === "on") {
-      var kitBtns = document.getElementsByClassName("kit-choice-btn");
-
-      for(let n = 0; n < kitBtns.length; n++) {
-        kitBtns[n].style.backgroundColor = "#b6b4be";
-        kitBtns[n].style.backgroundImage = "radial-gradient( rgba(192, 199, 202, 0.6), rgba(182, 180, 190, 0.6) )";
-      }
-
-      event.currentTarget.style.backgroundColor = "#dad9de";
-      event.currentTarget.style.backgroundImage = "radial-gradient( rgba(233, 232, 235, 0.8), rgba(218, 217, 222, 0.8) )";
+      // var kitBtns = document.getElementsByClassName("kit-choice-btn");
+      //
+      // for(let n = 0; n < kitBtns.length; n++) {
+      //   kitBtns[n].style.backgroundColor = "#b6b4be";
+      //   kitBtns[n].style.backgroundImage = "radial-gradient( rgba(192, 199, 202, 0.6), rgba(182, 180, 190, 0.6) )";
+      // }
+      //
+      // event.currentTarget.style.backgroundColor = "#dad9de";
+      // event.currentTarget.style.backgroundImage = "radial-gradient( rgba(233, 232, 235, 0.8), rgba(218, 217, 222, 0.8) )";
       event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0)";
 
       this.props.setCurrentKit(event);
@@ -26,6 +26,18 @@ class KitChoiceContainer extends React.Component {
 
   kitBtnUp(event) {
     event.currentTarget.style.boxShadow = "6px 6px 6px rgba(0,0,0, 1.0)";
+  }
+
+  kitBtnStyle(kit) {
+    if(this.props.power === "on" && this.props.currentKit === kit) {
+      return "kit-choice-btn ctrl-btn-active"
+    }
+    else if(this.props.power === "on" && this.props.currentKit !== kit) {
+      return "kit-choice-btn ctrl-btn-on"
+    }
+    else {
+      return "kit-choice-btn ctrl-btn-off"
+    }
   }
 
   handleKeyPress(event) {
@@ -53,14 +65,9 @@ class KitChoiceContainer extends React.Component {
   }
 
   render() {
-    let btnPowered = {};
     let btnGlowPowered = {};
 
     if(this.props.power === "off") {
-      btnPowered = {
-        backgroundImage: "none",
-        backgroundColor: "#898F90"
-      }
       btnGlowPowered = {
         boxShadow: "none",
         backgroundColor: "rgba(255,255,255, 0.0)"
@@ -70,19 +77,19 @@ class KitChoiceContainer extends React.Component {
     return (
       <div>
         <div id="kit-choice-container">
-          <div className="kit-choice-btn" id="kit-btn-1" style={btnPowered}
+          <div className={this.kitBtnStyle("kit1")} id="kit-btn-1"
           onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp} onMouseLeave={this.kitBtnUp}>
             <div className="kit-choice-btn-glow" style={btnGlowPowered}></div>
             <p>1</p>
           </div>
 
-          <div className="kit-choice-btn" id="kit-btn-2" style={btnPowered}
+          <div className={this.kitBtnStyle("kit2")} id="kit-btn-2"
           onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp} onMouseLeave={this.kitBtnUp}>
             <div className="kit-choice-btn-glow" style={btnGlowPowered}></div>
             <p>2</p>
           </div>
 
-          <div className="kit-choice-btn" id="kit-btn-3" style={btnPowered}
+          <div className={this.kitBtnStyle("kit3")} id="kit-btn-3"
           onMouseDown={this.kitBtnDown} onMouseUp={this.kitBtnUp} onMouseLeave={this.kitBtnUp}>
             <div className="kit-choice-btn-glow" style={btnGlowPowered}></div>
             <p>3</p>
