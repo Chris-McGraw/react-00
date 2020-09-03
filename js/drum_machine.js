f47457c520a0c9944c862c89b9c45e7edf9dc472
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-09-02 20:22:15 -0400

    localStorage testing

--- js/drum_machine.js
+++ js/drum_machine.js
@@ -126,13 +126,13 @@ class DrumMachine extends React.Component {
 
   deleteRecording(event) {
     if(this.state.power === "on" && this.state.nowRecording === false && this.state.nowPlaying === false && this.state.playbackArr.length > 0) {
+      clearLocalStorage("storedTrack1");
+
       this.setState({
         playbackArrPrevious: [],
         playbackArrUndone: [],
-        // playbackArr: []
+        playbackArr: JSON.parse(localStorage.getItem("storedTrack1"))
       });
-
-      clearLocalStorage("storedTrack1");
 
       event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0), inset 0 0 100px 100px rgba(255, 255, 255, 0.5)";
       console.log("RECORDING DELETED");
