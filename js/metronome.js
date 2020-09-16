e774f9bc10b3a523630617740753d9e81ace16f8
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-09-16 10:46:35 -0400

    edit Metronome component

--- js/metronome.js
+++ js/metronome.js
@@ -3,8 +3,17 @@
 class Metronome extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      metroBPM: 100
+    };
     this.metronomeToggle = this.metronomeToggle.bind(this);
     this.metronomeTimeout = null;
+    this.metroSpeedDown = this.metroSpeedDown.bind(this);
+    this.metroSpeedUp = this.metroSpeedUp.bind(this);
+  }
+
+  metroBtnUp(event) {
+    event.currentTarget.style.boxShadow = "6px 6px 6px rgba(0,0,0, 1.0), inset 0 0 0 0 rgba(255, 255, 255, 0.0)";
   }
 
   metronomeToggle(event) {
@@ -20,9 +29,11 @@ class Metronome extends React.Component {
         audio.play();
         console.log("metronome ticked");
 
+        event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0)";
+
         this.metronomeTimeout = setTimeout(function() {
           this.metronomeToggle();
-        }.bind(this), ((60 / 100) * 1000) );
+        }.bind(this), ((60 / this.state.metroBPM) * 1000) );
       }
       else if(this.props.metronomePlaying === true && event === undefined) {
         let audio = document.getElementById("metroAudio").cloneNode(true);
@@ -33,13 +44,35 @@ class Metronome extends React.Component {
 
         this.metronomeTimeout = setTimeout(function() {
           this.metronomeToggle();
-        }.bind(this), ((60 / 100) * 1000) );
+        }.bind(this), ((60 / this.state.metroBPM) * 1000) );
       }
       else if(this.props.metronomePlaying === true && event !== undefined) {
         clearTimeout(this.metronomeTimeout);
 
         this.props.toggleMetronomePlaying();
+
+        event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0)";
       }
+    }
+  }
+
+  metroSpeedDown(event) {
+    if(this.props.power === "on" && this.props.metronomePlaying === false) {
+      this.setState({
+        metroBPM: this.state.metroBPM - 1
+      });
+
+      event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0), inset 0 0 100px 100px rgba(255, 255, 255, 0.5)";
+    }
+  }
+
+  metroSpeedUp(event) {
+    if(this.props.power === "on" && this.props.metronomePlaying === false) {
+      this.setState({
+        metroBPM: this.state.metroBPM + 1
+      });
+
+      event.currentTarget.style.boxShadow = "4px 4px 6px rgba(0,0,0, 1.0), inset 0 0 100px 100px rgba(255, 255, 255, 0.5)";
     }
   }
 
@@ -71,22 +104,25 @@ class Metronome extends React.Component {
     return (
       <div>
         <div id="metronome-container">
-          <div id="metro-display">100 BPM</div>
+          <div id="metro-display">{this.state.metroBPM} BPM</div>
 
-          <div className={this.metroBtnStyle("metro-toggle-btn")} onMouseDown={this.metronomeToggle}>
+          <div className={this.metroBtnStyle("metro-toggle-btn")} onMouseDown={this.metronomeToggle}
+          onMouseUp={this.metroBtnUp} onMouseLeave={this.metroBtnUp}>
             <div className={this.metroGlowStyle("metro-toggle-btn")}>
               <p>M</p>
             </div>
             <audio preload="auto" src="audio/percs/tamby.mp3" id="metroAudio"></audio>
           </div>
 
-          <div className={this.metroBtnStyle()}>
+          <div className={this.metroBtnStyle()} onMouseDown={this.metroSpeedDown}
+          onMouseUp={this.metroBtnUp} onMouseLeave={this.metroBtnUp}>
             <div className={this.metroGlowStyle()}>
               <i className="fas fa-caret-down"></i>
             </div>
           </div>
 
-          <div className={this.metroBtnStyle()}>
+          <div className={this.metroBtnStyle()} onMouseDown={this.metroSpeedUp}
+          onMouseUp={this.metroBtnUp} onMouseLeave={this.metroBtnUp}>
             <div className={this.metroGlowStyle()}>
               <i className="fas fa-caret-up"></i>
             </div>
